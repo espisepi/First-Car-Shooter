7f8eb49b8901b996d4ac2feda4dd76b376a3e609
espisepi <sepinaco@icloud.com>
2023-08-29 16:01:42 +0200

    vehiclespawnpoint descomented

--- src/client/espinaco-features/sketchbook/world/VehicleSpawnPoint.ts
+++ src/client/espinaco-features/sketchbook/world/VehicleSpawnPoint.ts
@@ -1,20 +1,18 @@
 import * as THREE from 'three'
 import { ISpawnPoint } from '../interfaces/ISpawnPoint'
 import { World } from '../world/World'
-// sepinaco commented
-// import { Helicopter } from '../vehicles/Helicopter';
-// import { Airplane } from '../vehicles/Airplane';
-// import { Car } from '../vehicles/Car';
+import { Helicopter } from '../vehicles/Helicopter'
+import { Airplane } from '../vehicles/Airplane'
+import { Car } from '../vehicles/Car'
 import * as Utils from '../core/FunctionLibrary'
-// sepinaco commented
-// import { Vehicle } from '../vehicles/Vehicle';
-// import { Character } from '../characters/Character';
-// import { FollowPath } from '../characters/character_ai/FollowPath';
+import { Vehicle } from '../vehicles/Vehicle'
+import { Character } from '../characters/Character'
+import { FollowPath } from '../characters/character_ai/FollowPath'
 import { LoadingManager } from '../core/LoadingManager'
 import { IWorldEntity } from '../interfaces/IWorldEntity'
 
 export class VehicleSpawnPoint implements ISpawnPoint {
-    public type?: string
+    public type: string = 'undefined'
     public driver?: string
     public firstAINode?: string
 
@@ -28,68 +26,96 @@ export class VehicleSpawnPoint implements ISpawnPoint {
         loadingManager.loadGLTF(
             'build/assets/' + this.type + '.glb',
             (model: any) => {
-                // sepinaco commented
-                // let vehicle: Vehicle = this.getNewVehicleByType(model, this.type);
-                // vehicle.spawnPoint = this.object;
-                // let worldPos = new THREE.Vector3();
-                // let worldQuat = new THREE.Quaternion();
-                // this.object.getWorldPosition(worldPos);
-                // this.object.getWorldQuaternion(worldQuat);
-                // vehicle.setPosition(worldPos.x, worldPos.y + 1, worldPos.z);
-                // vehicle.collision.quaternion.copy(Utils.cannonQuat(worldQuat));
-                // world.add(vehicle);
-                // if (this.driver !== undefined)
-                // {
-                // 	loadingManager.loadGLTF('build/assets/boxman.glb', (charModel) =>
-                // 	{
-                // 		let character = new Character(charModel);
-                // 		world.add(character);
-                // 		character.teleportToVehicle(vehicle, vehicle.seats[0]);
-                // 		if (this.driver === 'player')
-                // 		{
-                // 			character.takeControl();
-                // 		}
-                // 		else if (this.driver === 'ai')
-                // 		{
-                // 			if (this.firstAINode !== undefined)
-                // 			{
-                // 				let nodeFound = false;
-                // 				for (const pathName in world.paths) {
-                // 					if (world.paths.hasOwnProperty(pathName)) {
-                // 						const path = world.paths[pathName];
-                // 						for (const nodeName in path.nodes) {
-                // 							if (Object.prototype.hasOwnProperty.call(path.nodes, nodeName)) {
-                // 								const node = path.nodes[nodeName];
-                // 								if (node.object.name === this.firstAINode)
-                // 								{
-                // 									character.setBehaviour(new FollowPath(node, 10));
-                // 									nodeFound = true;
-                // 								}
-                // 							}
-                // 						}
-                // 					}
-                // 				}
-                // 				if (!nodeFound)
-                // 				{
-                // 					console.error('Path node ' + this.firstAINode + 'not found.');
-                // 				}
-                // 			}
-                // 		}
-                // 	});
-                // }
+                let vehicleOrNot: Vehicle | null = this.getNewVehicleByType(
+                    model,
+                    this.type
+                )
+                if (vehicleOrNot) {
+                    let vehicle = vehicleOrNot
+                    vehicle.spawnPoint = this.object
+                    let worldPos = new THREE.Vector3()
+                    let worldQuat = new THREE.Quaternion()
+                    this.object.getWorldPosition(worldPos)
+                    this.object.getWorldQuaternion(worldQuat)
+                    vehicle.setPosition(worldPos.x, worldPos.y + 1, worldPos.z)
+                    vehicle.collision.quaternion.copy(Utils.cannonQuat(worldQuat))
+                    world.add(vehicle)
+                    if (this.driver !== undefined) {
+                        loadingManager.loadGLTF(
+                            'build/assets/boxman.glb',
+                            (charModel) => {
+                                let character = new Character(charModel)
+                                world.add(character)
+                                character.teleportToVehicle(
+                                    vehicle,
+                                    vehicle.seats[0]
+                                )
+                                if (this.driver === 'player') {
+                                    character.takeControl()
+                                } else if (this.driver === 'ai') {
+                                    if (this.firstAINode !== undefined) {
+                                        let nodeFound = false
+                                        for (const pathName in world.paths) {
+                                            if (
+                                                world.paths.hasOwnProperty(pathName)
+                                            ) {
+                                                const path = world.paths[pathName]
+                                                for (const nodeName in path.nodes) {
+                                                    if (
+                                                        Object.prototype.hasOwnProperty.call(
+                                                            path.nodes,
+                                                            nodeName
+                                                        )
+                                                    ) {
+                                                        const node =
+                                                            path.nodes[nodeName]
+                                                        if (
+                                                            node.object.name ===
+                                                            this.firstAINode
+                                                        ) {
+                                                            character.setBehaviour(
+                                                                new FollowPath(
+                                                                    node,
+                                                                    10
+                                                                )
+                                                            )
+                                                            nodeFound = true
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                        if (!nodeFound) {
+                                            console.error(
+                                                'Path node ' +
+                                                    this.firstAINode +
+                                                    'not found.'
+                                            )
+                                        }
+                                    }
+                                }
+                            }
+                        )
+                    }
+                } else {
+                    console.warn(
+                        'Aviso que el metodo getNewVehicleByType ha devuelto null, es decir, no ha encontrado ningun vehiculo'
+                    )
+                }
             }
         )
     }
 
-    // sepinaco commented
-    // private getNewVehicleByType(model: any, type: string): Vehicle {
-    //     switch (type) {
-    //         case 'car':
-    //             return new Car(model)
-    //         case 'heli':
-    //             return new Helicopter(model)
-    //         case 'airplane':
-    //             return new Airplane(model)
-    //     }
-    // }
+    private getNewVehicleByType(model: any, type: string): Vehicle | null {
+        switch (type) {
+            case 'car':
+                return new Car(model)
+            case 'heli':
+                return new Helicopter(model)
+            case 'airplane':
+                return new Airplane(model)
+            default:
+                return null
+        }
+    }
 }
